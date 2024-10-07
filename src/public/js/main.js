dbd569f6e76bbb04f2f9841fdbac90ee0668763d
renatamandelman <renatamandel@gmail.com>
2024-10-07 19:55:31 -0300

    segEntregaa

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -4,5 +4,8 @@ console.log("Si funciona");
 socket.on("products", data => {
     renderProducts(data);
 })
-
+//renderizar en pnatalla los productos
+//sumar un boton para eliminar productos
+//sumar un form para crear neuevos productos
+//todo esto tiene que actualizar en tiempo real
 
